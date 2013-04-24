50c61571bba4c9ffe0bde8b426cee9b9f612db11
Thai Le <lnthai2002@gmail.com>
2013-04-24 19:13:22 -0400

    issue_44: Copy controller from PAS

---
+++ spec/controllers/cooking/ingredients_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module Cooking
+  describe IngredientsController do
+  
+  end
+end
---
+++ spec/controllers/cooking/recipes_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+module Cooking
+  describe RecipesController do
+  
+  end
+end
