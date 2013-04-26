9e43b04556af8fc818c9d4fa430375f2f1890c5e
Thai Le <lnthai2002@gmail.com>
2013-04-25 22:21:14 -0400

    issue_44: Common base model for this engine should be abstract class

--- app/models/cooking/base_model.rb
+++ app/models/cooking/base_model.rb
@@ -1,5 +1,6 @@
 module Cooking
   class BaseModel < ActiveRecord::Base
+    self.abstract_class = true
     ActiveRecord::Base.establish_connection Rails.configuration.database_configuration['cooking']
   end
 end
