4e602e62a8db8b063f607f5a54a2d1512fc9088f
Thai Le <lnthai2002@gmail.com>
2013-04-25 22:04:23 -0400

    issue_44: Namespace model specs

--- spec/models/cooking/ingredient_spec.rb
+++ spec/models/cooking/ingredient_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
-describe Ingredient do
-  it 'should belongs to a recipe' do
-    ingredient = Ingredient.new
-    ingredient.should be_respond_to(:recipes)
-  end
-
-  it 'should have an image' do
-    ingredient = Ingredient.new
-    ingredient.should be_respond_to(:image)
+module Cooking
+  describe Ingredient do
+    it 'should belongs to a recipe' do
+      ingredient = Ingredient.new
+      ingredient.should be_respond_to(:recipes)
+    end
+  
+    it 'should have an image' do
+      ingredient = Ingredient.new
+      ingredient.should be_respond_to(:image)
+    end
   end
 end
