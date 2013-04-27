717e15f6b78a55915f04fe75ab91366bc0dbbf62
Thai Le <lnthai2002@gmail.com>
2013-04-27 09:04:28 -0400

    issue_44: Copy cooking models from PAS

---
+++ app/models/cooking/recipe.rb
@@ -0,0 +1,12 @@
+module Cooking
+  class Recipe < ActiveRecord::Base
+    # attr_accessible :title, :body
+    has_many :steps, :dependent=>:destroy
+    has_many :quantified_ingredients
+    has_many :ingredients, :through=>:quantified_ingredients
+    accepts_nested_attributes_for :steps,:reject_if => lambda { |attrs| attrs.all? { |key, value| value.blank? }}
+    accepts_nested_attributes_for :quantified_ingredients,:reject_if => lambda { |attrs| attrs.all? { |key, value| value.blank? }}
+    #Use RecipeImageUploader to upload image and record path to image column
+    mount_uploader :image, RecipeImageUploader
+  end
+end
