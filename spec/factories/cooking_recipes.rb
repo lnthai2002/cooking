a63b6fe596d94be53dcdafe6f9c7fd264ba53a53
Thai Le <lnthai2002@gmail.com>
2013-04-27 09:11:19 -0400

    issue_44: Copy factories from PAS, also include some factories generated with this engine (should be empty)

---
+++ spec/factories/cooking_recipes.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :cooking_recipe, :class => 'Recipe' do
+    name 'recipe 1'
+    description 'description for recipe 1'
+  end
+end
