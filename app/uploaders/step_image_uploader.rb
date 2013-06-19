9a19b050f709653372112dfbe61287c90c9d8edc
Thai Le <lnthai2002@gmail.com>
2013-06-19 13:48:05 -0400

    issue_44: namespace uploaders, change upload folder to match PAS deployment script

--- app/uploaders/step_image_uploader.rb
+++ app/uploaders/step_image_uploader.rb
@@ -1,5 +1,5 @@
 # encoding: utf-8
-
+module Cooking
 class StepImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
@@ -17,7 +17,7 @@ class StepImageUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "cooking_uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -60,4 +60,5 @@ class StepImageUploader < CarrierWave::Uploader::Base
   #   "something.jpg" if original_filename
   # end
 
+end
 end
