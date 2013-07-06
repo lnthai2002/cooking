377252f5244469f58ec06bbad4525c3289b22b5d
Thai Le <lnthai2002@gmail.com>
2013-07-06 10:43:50 -0400

    issue_77: bring image from PAS

--- app/helpers/cooking/application_helper.rb
+++ app/helpers/cooking/application_helper.rb
@@ -2,7 +2,7 @@ module Cooking
   module ApplicationHelper
     #Overide default image_tag to fall back to error image to avoid compiled blank image not found in production 
     def image_tag(source, options={})
-      source = "image_not_available.png" if source.blank?
+      source = "cooking/image_not_available.png" if source.blank?
       super(source, options)
     end
   end
