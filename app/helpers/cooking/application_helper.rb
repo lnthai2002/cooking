daddfa1424e585ac3b32693a2a426b6882a19176
Thai Le <lnthai2002@gmail.com>
2014-02-17 20:14:19 -0500

    issue_177: highlight current action in the action menu

--- app/helpers/cooking/application_helper.rb
+++ app/helpers/cooking/application_helper.rb
@@ -1,4 +1,21 @@
 module Cooking
+  class MenuTabBuilder < TabsOnRails::Tabs::Builder
+    def open_tabs(options = {})
+      @context.tag('ul', options, open = true)
+    end
+  
+    def close_tabs(options = {})
+      '</ul>'.html_safe
+    end
+  
+    def tab_for(tab, name, options, item_options = {})
+      item_options[:class] = (current_tab?(tab) ? 'active' : '')
+      @context.content_tag('li', item_options) do
+        @context.link_to(name, options)
+      end
+    end
+  end
+
   module ApplicationHelper
     #Overide default image_tag to fall back to error image to avoid compiled blank image not found in production 
     def image_tag(source, options={})
