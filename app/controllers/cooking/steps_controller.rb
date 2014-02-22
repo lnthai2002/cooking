9f5cdc912341203540280b1ba5d9f7c93e8726bd
Thai Le <lnthai2002@gmail.com>
2014-02-22 16:54:58 -0500

    issue_177: add, edit steps using modal

--- app/controllers/cooking/steps_controller.rb
+++ app/controllers/cooking/steps_controller.rb
@@ -2,117 +2,107 @@ require_dependency "cooking/application_controller"
 
 module Cooking
   class StepsController < ApplicationController
-    before_filter :find_recipe
-    before_filter :find_step, :only => [:show, :edit, :update, :destroy]
+    before_action :find_recipe
+    before_action :find_step, only: [:show, :edit, :update, :destroy]
 
-    # GET /steps
-    # GET /steps.json
+    # GET /recipes/1/steps
     def index
       @recipe = Recipe.find(params[:recipe_id])
       @steps = @recipe.steps.to_a
-  
+
       respond_to do |format|
-        format.html # index.html.erb
-        format.json { render json: @steps }
-      end
-    end
-=begin  
-    # GET /steps/1
-    # GET /steps/1.json
-    def show
-      @step = Step.find(params[:id])
-  
-      respond_to do |format|
-        format.html # show.html.erb
-        format.json { render json: @step }
-      end
-    end
-=end
-    # GET /steps/new
-    # GET /steps/new.json
-    def new
-      @step = @recipe.steps.build
-  
-      respond_to do |format|
-        format.html # new.html.erb
-        format.js {render 'form'}
-        format.json { render json: @step }
+        format.html
       end
     end
 
+    # GET /recipes/1/steps/new.js
+    def new
+      @step = @recipe.steps.build
+      respond_to do |format|
+        format.js {render 'show_form'}
+      end
+    end
+
+    # GET /recipes/1/steps/2/edit.js
     def edit
-      @recipe = Recipe.find(params[:recipe_id])
-      @step = Step.find(params[:id])
       respond_to do |format|
-        format.js {render 'form'}
+        format.js {render 'show_form'}
       end
     end
-  
-    # POST /steps
-    # POST /steps.json
+
+    # POST /recipes/1/steps.js
     def create
-      @step = @recipe.steps.build(params[:step])
-  
-      if @step.save
-        build_new_step
-        render :update
-      else
-        
-      end
-    end
-  
-    # PUT /steps/1
-    # PUT /steps/1.json
-    def update
-      if @step.update_attributes(params[:step])
-        build_new_step
-        render :update
-      else
-        
-      end
-      
-    end
-  
-    # DELETE /steps/1
-    # DELETE /steps/1.json
-    def destroy
-  #    @step = Step.find(params[:id])
-      @step.destroy
-  
+      @step = @recipe.steps.build(step_params)
+
       respond_to do |format|
-        format.html { redirect_to @recipe }
-        format.json { head :ok }
-      end
-    end
-  
-    #This should be called using
-    def update_sequences
-      begin
-        Step.transaction  do #update all or nothing
-          params[:recordid].each_with_index do |id, index|
-            Step.update_all("sequence_number = #{index + 1}", ["id = ?", id])
-          end
+        if @step.save
+          flash[:notice] = "Step #{@step.sequence_number} added"
+          format.js {render 'reload_list'}
+        else
+          format.js {render 'show_form'}
         end
-      rescue Exception=>e
-        notice = 'Cannot update sequence, there is something wrong, developer has been notified'
-      end
-      respond_to do |format|
-        format.html {redirect_to :controller=>:recipes, :action=>:show, :id=>@recipe.id, :format => 'html'}
-        format.xml  { head :ok }
-        format.json { head :ok }
       end
     end
-  
-private
+
+    # PATCH /recipes/1/steps/2.js
+    def update
+      respond_to do |format|
+        if @step.update_attributes(step_params)
+          flash[:notice] = "Step #{@step.sequence_number} updated"
+          format.js {render 'reload_list'}
+        else
+          format.js {render 'show_form'}
+        end
+      end
+    end
+
+    # DELETE /recipes/1/steps/2.js
+    def destroy
+      respond_to do |format|
+        if @step.destroy
+          #TODO: re-sort all sequence numbers of steps
+          flash[:notice] = "Step removed"
+          format.js { render 'reload_list'}
+        else
+          flash[:alert] = "Cannot remove this step!"
+          format.js { render 'show_dialog'}
+        end
+      end
+    end
+
+    # PATCH /cooking/recipes/14/steps/update_sequences.json
+    def update_sequences
+      respond_to do |format|
+        begin
+          Step.transaction do #update all or nothing
+            params[:recordid].each_with_index do |id, index|
+              Step.update_all("sequence_number = #{index + 1}", ["id = ?", id])
+            end
+          end
+          format.json { head :ok }
+        rescue Exception=>e
+          notice = 'Cannot update sequence, there is something wrong, developer has been notified'
+          format.json { head 400 }
+        end
+      end
+    end
+
+    private
+
+    def step_params
+      params.require(:step)
+            .permit(:description, :image, :sequence_number)
+    end
+
     def build_new_step
       #build a new step, this new one does not have sequence_id YET
       @step = @recipe.steps.build
     end
-  
+
     def find_recipe
       @recipe = Recipe.find(params[:recipe_id])
     end
-    
+
     def find_step
       @step = @recipe.steps.find(params[:id])
     end
