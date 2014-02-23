92d9323305e9d1d0ebad95fb72b52e34762f5692
Thai Le <lnthai2002@gmail.com>
2014-02-22 22:35:49 -0500

    issue_177: use modal for edit/create quantified ingredient

--- app/controllers/cooking/quantified_ingredients_controller.rb
+++ app/controllers/cooking/quantified_ingredients_controller.rb
@@ -2,86 +2,68 @@ require_dependency "cooking/application_controller"
 
 module Cooking
   class QuantifiedIngredientsController < ApplicationController
-    def new
-      @recipe = Recipe.find(params[:recipe_id])
-      @quantified_ingredient = @recipe.quantified_ingredients.build #build new ingredient
-      @quantified_ingredient.ingredient = Ingredient.new
+    before_action :find_recipe
+    before_action :find_quantified_ingredient, only:[:destroy,:edit,:update]
 
+    def new
+      @quantified_ingredient = @recipe.quantified_ingredients.build
+      @quantified_ingredient.ingredient = Ingredient.new
       respond_to do |format|
-        format.js  {render 'form'}
+        format.js { render 'show_form' }
       end
     end
 
     def create
-      @recipe = Recipe.find(params[:recipe_id])
       @quantified_ingredient = QuantifiedIngredient.new(quantified_ingredient_params)
       @quantified_ingredient.recipe_id = params[:recipe_id]
-
       respond_to do |format|
         if @quantified_ingredient.save
           flash[:notice] = "#{@quantified_ingredient.ingredient.name} added"
-          format.js { render 'cooking/quantified_ingredients/created'}
+          format.js { render 'reload_list'}
         else
-          format.js { render 'cooking/quantified_ingredients/form'}
+          format.js { render 'show_form'}
         end
       end
     end
   
     def destroy
-      if not params[:recipe_id].blank?
-        delete_and_forward_to_recipe
-      else
-        delete_and_go_back_to_index
+      respond_to do |format|
+        if @quantified_ingredient.destroy
+          flash[:notice] = "#{@quantified_ingredient.ingredient.name} removed"
+          format.js { render 'reload_list' }
+        else
+          flash[:alert] = "#{@quantified_ingredient.ingredient.name} cannot be removed"
+          format.js { render 'show_dialog' }
+        end
       end
     end
 
     def edit
-      @recipe = Recipe.find(params[:recipe_id])
-      @quantified_ingredient = @recipe.quantified_ingredients.find(params[:id])
       respond_to do |format|
-        format.js  {render 'form'}
+        format.js  {render 'show_form'}
       end
     end
 
     def update
-      @recipe = Recipe.find(params[:recipe_id])
-      @quantified_ingredient = @recipe.quantified_ingredients.find(params[:id])
-
       respond_to do |format|
         if @quantified_ingredient.update_attributes(quantified_ingredient_params)
           flash[:notice] = "#{@quantified_ingredient.ingredient.name} updated"
-          format.js { render 'cooking/quantified_ingredients/created'}
+          format.js { render 'reload_list' }
         else
-          format.js { render 'cooking/quantified_ingredients/form'}
+          format.js { render 'show_form' }
         end
-      end
-    end
-private
-    def create_and_forward_to_recipe
-      quantified_ingredient = QuantifiedIngredient.new(params[:quantified_ingredient])
-      quantified_ingredient.recipe_id = params[:recipe_id]
-      respond_to do |format|
-        if quantified_ingredient.save
-          format.html { redirect_to edit_recipe_path(params[:recipe_id]), notice: 'An ingredient was successfully added.' }
-          format.json { render json: quantified_ingredient, status: :created, location: quantified_ingredient }
-        else
-          format.html { redirect_to edit_recipe_path(params[:recipe_id]), notice: 'Error when creating ingredient' }
-          format.json { render json: quantified_ingredient.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  
-    def delete_and_forward_to_recipe
-      #TODO: also remove ingredient if there is no quantifiedIngrdient associate with it
-      QuantifiedIngredient.find(params[:id]).destroy
-  
-      respond_to do |format|
-        format.html { redirect_to edit_recipe_path(params[:recipe_id]) }
-        format.json { head :ok }
       end
     end
 
     private
+
+    def find_recipe
+      @recipe = Recipe.find(params[:recipe_id])
+    end
+
+    def find_quantified_ingredient
+      @quantified_ingredient = @recipe.quantified_ingredients.find(params[:id])
+    end
 
     def quantified_ingredient_params
       params.require(:quantified_ingredient)
